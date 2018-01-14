5a0b663e780d400040acb842a2d1bc631ba04d59
Jessica Ete <kohrVid@zoho.com>
2018-01-14 20:41:22 +0000

    Adding GraphQL request to app
    Still unable to set up config variables with typesafe library and unable
    to move the text box for the response data to a new row using ScalaFX.

--- build.sbt
+++ build.sbt
@@ -4,10 +4,15 @@ organization := "kohrVid"
 
 version := "0.1-SNAPSHOT"
 
-scalaVersion := "2.12.1"
+scalaVersion := "2.12.2"
+
+resolvers += Resolver.bintrayRepo("findify", "maven")
 
 libraryDependencies ++= Seq(
   "org.scalafx"   %% "scalafx"   % "8.0.102-R11",
+  "org.scalaj" %% "scalaj-http" % "2.3.0",
+  "org.json4s" %% "json4s-native" % "3.5.0",
+  "com.typesafe" %% "config" % "1.3.1",
   "org.scalatest" %% "scalatest" % "3.0.1" % "test" //http://www.scalatest.org/download
 )
 
@@ -16,6 +21,7 @@ shellPrompt := { state => System.getProperty("user.name") + "> " }
 // set the main class for the main 'run' task
 // change Compile to Test to set it for 'test:run'
 mainClass in (Compile, run) := Some("favourite.languages.ScalaFXHelloWorld")
+
 
 // Fork a new JVM for 'run' and 'test:run' to avoid JavaFX double initialization problems
 fork := true
--- src/main/scala/favourite/languages/ScalaFXHelloWorld.scala
+++ src/main/scala/favourite/languages/ScalaFXHelloWorld.scala
@@ -9,40 +9,68 @@ import scalafx.scene.layout.HBox
 import scalafx.scene.paint.Color._
 import scalafx.scene.paint._
 import scalafx.scene.text.Text
+import scalafx.scene.control.{Dialog, TextField}
+
+import java.io._
+import scalaj.http._
+import org.json4s.DefaultFormats
+import org.json4s.native.Serialization
+
+import com.typesafe.config.ConfigFactory
 
 object ScalaFXHelloWorld extends JFXApp {
 
   stage = new PrimaryStage {
-    //    initStyle(StageStyle.UNIFIED)
-    title = "ScalaFX Hello World"
+    title = "Favourite Language"
+    width = 450
+    height = 150
     scene = new Scene {
       fill = Color.rgb(38, 38, 38)
       content = new HBox {
         padding = Insets(50, 80, 50, 80)
         children = Seq(
           new Text {
-            text = "Scala"
-            style = "-fx-font: normal bold 100pt sans-serif"
+            text = "Username"
+            style = "-fx-font: normal bold 14pt sans-serif"
             fill = new LinearGradient(
               endX = 0,
-              stops = Stops(Color.Red, Color.DarkRed))
+              stops = Stops(Color.LightGray, Color.DarkGray))
+            spacing = 15
+          },
+          new TextField {
+            prefColumnCount = 10
           },
           new Text {
-            text = "FX"
-            style = "-fx-font: italic bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(White, DarkGray)
-            )
-            effect = new DropShadow {
-              color = DarkGray
-              radius = 15
-              spread = 0.25
-            }
+            val config = loadConfigOrThrow[Config](ConfigFactory.load.getConfig("favourite.languages"))
+            println(s"CONFIG --> $config")
+	    text = repoData("kohrVid", config.githubAuth)
           }
         )
       }
     }
+  }
 
+  def repoData(username: String, auth: String): String = {
+    implicit val formats = DefaultFormats
+
+    // Why is string interpolation such a bitch?
+    val languages = "{\n edges {\n size\n node {\nid\nname\ncolor\n}\n  }\n}"
+    val repositories = "repositories(first: 100, after: \""+ auth + "\") {\n edges {\n   cursor\n   node {\nname\nlanguages(first: 20) \n" + languages + "}\n }\n    }"
+    val q = "{\n  user(login: \"" + username + "\") {\n    name\n" + repositories + "\n  }\n}\n"
+
+    val query = Serialization.write(
+      Map(
+        "query" -> q,
+        "variables" -> "{}",
+        "operationName" -> null
+      )
+    )
+
+    val response: HttpResponse[String] = Http(
+    "https://api.github.com/graphql"
+    ).headers(Seq(
+      "Authorization" -> s"bearer $auth"
+    )).postData(query).asString
+    response.body
   }
 }
